06cf2752433c967682097113ecfa56412248fad2
farhikhta farzan <farhikhtafarzan@gmail.com>
2024-03-02 23:15:56 -0500

    homepage updated and navigation added to files

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,5 @@
 const explore_button = document.querySelectorAll('.explore-btn');
+const nav = document.querySelector('.nav')
 
 // ripple effect for the explore-btn 
 explore_button.forEach(button => {
@@ -21,3 +22,20 @@ explore_button.forEach(button => {
         this.appendChild(circle)
     })
 })
+
+// navigation effect
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    console.log(window.scrollY, nav.offsetHeight)
+    if(window.scrollY > nav.offsetHeight + 150){
+        nav.classList.add('active');
+    }
+    else{
+        nav.classList.remove('active');
+    }
+
+    if(nav.classList.contains('active') && window.scrollY > nav.offsetHeight +1000) {
+        nav.classList.remove('active');
+    }
+}
