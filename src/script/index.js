c63dd89ce0a0971d5c1d4b772d3f401241c6f027
farhikhta farzan <farhikhtafarzan@gmail.com>
2024-03-06 01:13:44 -0500

    homepage carousel and wave added

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,9 @@
 const explore_button = document.querySelectorAll('.explore-btn');
 const nav = document.querySelector('.nav')
+const carousel_imgs = document.getElementById('imgs')
+const carousel_left_btn = document.getElementById('left')
+const carousel_right_btn = document.getElementById('right')
+const carousel_each_img = document.querySelectorAll('#imgs img')
 
 // ripple effect for the explore-btn 
 explore_button.forEach(button => {
@@ -28,14 +32,60 @@ window.addEventListener('scroll', fixNav)
 
 function fixNav() {
     console.log(window.scrollY, nav.offsetHeight)
-    if(window.scrollY > nav.offsetHeight + 150){
+    if(window.scrollY > nav.offsetHeight + 1050){
         nav.classList.add('active');
     }
     else{
         nav.classList.remove('active');
     }
 
-    if(nav.classList.contains('active') && window.scrollY > nav.offsetHeight +1000) {
+    if(nav.classList.contains('active') && window.scrollY > nav.offsetHeight +2500) {
         nav.classList.remove('active');
     }
 }
+
+//carousel
+let index = 0
+let interval = setInterval(run, 2000)
+
+function run() {
+    index++;
+    changeImage()
+}
+
+function changeImage() {
+    if(index > carousel_each_img.length - 1) {
+        index = 0
+    }
+    else if (index < 0) {
+        index = carousel_each_img.length - 1
+    }
+
+    if(window.innerWidth >= 300 && window.innerWidth <= 600) {
+        console.log(innerWidth)
+        carousel_imgs.style.transform = `translateX(${-index*236}px)`
+    }
+    else if(window.innerWidth >= 700 && window.innerWidth <= 1000) {
+        carousel_imgs.style.transform = `translateX(${-index*339}px)`
+    }
+    else {
+        carousel_imgs.style.transform = `translateX(${-index*339}px)`
+    }
+    
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
+carousel_right_btn.addEventListener('click', ()=> {
+    index++
+    changeImage()
+    resetInterval()
+})
+
+carousel_left_btn.addEventListener('click', ()=> {
+    index--
+    changeImage()
+    resetInterval()
+})
